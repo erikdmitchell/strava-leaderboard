7f04674fb98a9410ac53e61537b7e5ac17ceceb4
Erik <erikdmitchell@gmail.com>
2020-11-03 13:28:23 -0500

    updated leaderboard template

--- templates/content-leaderboard.php
+++ templates/content-leaderboard.php
@@ -2,40 +2,44 @@
 
 <div id="leaderboard-<?php the_ID(); ?>" class="slwp-leaderboard leaderboard">
 
-	<div class="summary entry-summary">
+	<div class="summary entry-summary slwp">
+        <?php the_title( '<h1>', '</h1>' ); ?>
 
-
-<div class="slwp">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h3>Segment Efforts</h3>
-            </div>
-        </div>
-        <?php echo $data['name']; ?>
-        <?php foreach($data['efforts'] as $effort) : ?>
+        <div class="container">
             <div class="row">
                 <div class="col">
-                    Time <?php echo $effort['time']; ?>
-                    <?php echo $effort['iskom']; ?>
-                </div>
-                <div class="col">
-                    <?php echo $effort['date']; ?>
-                </div>
-                <div class="col">
-                    <?php echo $effort['activityurl']; ?>
-                </div>
-                <div class="col">
-                    <?php echo $effort['komrank']; ?><br />
-                    <?php echo $effort['prrank']; ?>
+                    <?php the_content( ); ?>
                 </div>
             </div>
-        <?php endforeach; ?>
-    </div>
-</div>
+        </div>
 
-	
-	
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3>Segment Efforts</h3>
+                </div>
+            </div>
+            <?php echo $data['name']; ?>
+            <?php foreach($data['efforts'] as $effort) : ?>
+                <div class="row">
+                    <div class="col">
+                        Time <?php echo $effort['time']; ?>
+                        <?php echo $effort['iskom']; ?>
+                    </div>
+                    <div class="col">
+                        <?php echo $effort['date']; ?>
+                    </div>
+                    <div class="col">
+                        <?php echo $effort['activityurl']; ?>
+                    </div>
+                    <div class="col">
+                        <?php echo $effort['komrank']; ?><br />
+                        <?php echo $effort['prrank']; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
 	</div>
 
 </div>
