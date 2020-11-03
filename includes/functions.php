cedef9bf01189a4bea735f06d43fbd851602675b
Erik <erikdmitchell@gmail.com>
2020-11-03 12:28:27 -0500

    added get template part func

--- includes/functions.php
+++ includes/functions.php
@@ -14,3 +14,40 @@ function slwp_check_url_message() {
     }
 }
 add_action( 'wp_footer', 'slwp_check_url_message' );
+
+function slwp_get_template_part( $slug, $name = '' ) {
+    $template = false; // this needs to check for cache at some point. 
+    
+	if ( ! $template ) {
+		if ( $name ) {    		
+			$template = locate_template(
+				array(
+					"{$slug}-{$name}.php",
+					SLWP_PATH . "{$slug}-{$name}.php",
+				)
+			);
+
+			if ( ! $template ) {
+				$fallback = SLWP_PATH . "/templates/{$slug}-{$name}.php";
+				$template = file_exists( $fallback ) ? $fallback : '';
+			}
+		}
+
+		if ( ! $template ) {
+			// If template file doesn't exist, look in yourtheme/slug.php and yourtheme/slwp/slug.php.
+			$template = locate_template(
+				array(
+					"{$slug}.php",
+					$template_path . "{$slug}.php",
+				)
+			);			
+		}
+	}
+
+	// Allow 3rd party plugins to filter template file from their plugin.
+	$template = apply_filters( 'slwp_get_template_part', $template, $slug, $name );
+
+	if ( $template ) {
+		load_template( $template, false );
+	}
+}
