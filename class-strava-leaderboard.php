d02a486b00940ff787987c554c7093f2c728b411
Erik <erikdmitchell@gmail.com>
2020-11-05 16:45:48 -0500

    added temp token refresh

--- class-strava-leaderboard.php
+++ class-strava-leaderboard.php
@@ -116,6 +116,9 @@ final class SLWP {
         if ( is_admin() ) {
             include_once( SLWP_PATH . 'includes/admin/class-slwp-admin.php' );
         }
+        
+        //$utr = new SLWP_Users_Token_Refresh();
+        //$utr->check_users_token();
     }
 
     /**
