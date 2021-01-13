4cc115694bae061547c6da086ab69556a2694520
Erik <erikdmitchell@gmail.com>
2021-01-13 10:13:53 -0500

    added json return to webhooks class

--- includes/class-slwp-webhooks.php
+++ includes/class-slwp-webhooks.php
@@ -7,7 +7,7 @@ class SLWP_Webhooks {
     }
 
     public function create_subscription() {
-        $this->request_subscription();
+        //$this->request_subscription();
     }
 
     private function request_subscription() {
@@ -18,8 +18,10 @@ class SLWP_Webhooks {
         $callback_url = esc_url( home_url( '/slwp/stravaWebhooks' ) );
         $verify_token = 'slwplb';
         
+        $callback_url = urlencode( $callback_url );
+        
         $params = "client_id=$client_id&client_secret=$client_secret&callback_url=$callback_url&verify_token=$verify_token";
-echo $params.'<br>';
+//echo $params.'<br>';
 		//. "&redirect_uri=" . urlencode(SF_REDIRECT_URI);
 
         $curl=curl_init($url);
@@ -32,21 +34,87 @@ echo $params.'<br>';
     	$json_response = curl_exec($curl);
     
     	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    	
-print_r($status);    
-    	if ($status!=200) :		
+//echo '<pre>';    	
+//print_r($status); 
+slwp_log('Request subscription');
+slwp_log($status);   
+    	if ($status!=200) :
+//echo "error";    			
     	endif;
     
     	curl_close($curl);
     
     	$response = json_decode($json_response, true);
-print_r($json_response);    	
+slwp_log($response);
+    	
+//print_r($response);    	
+//echo '</pre>';
     }
     
     public function validation() {
-echo "validate webhooks<br>";        
+//slwp_log('Validate Webhooks');
+//slwp_log($_GET);        
+//echo "validate webhooks<br>";        
+
+//?hub.verify_token= slwplb&hub.challenge= $_GET['hub.challenge'] &hub.mode=subscribe
+/*
+Your callback address must respond within two seconds to the GET request from Strava’s subscription service. The response should indicate status code 200 and should echo the hub.challenge field in the response body as application/json content type: { “hub.challenge”:”15f7d1a91c1f40f8a748fd134752feb3” }
+
+Once you have successfully created a webhook events subscription by responding to the callback validation, you will receive a response to your original subscription creation POST request. This response will include the id of the newly created subscription. If creation of a new subscription fails, this response will instead include error information. The most common cause of subscription creation failure is a failure to respond in a timely manner to the validation GET request, or failure to correctly echo the hub.challenge field.
+
+Example Subscription Creation Response
+{
+  "id": 1
+}    
+*/
+
+/*
+    // clear the old headers
+    header_remove();
+    // set the actual code
+    http_response_code(200);
+    
+    header('Access-Control-Allow-Origin: *');
+    
+    // set the header to make sure cache is forced
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+    // treat this as json
+    header('Content-type:application/json;charset=utf-8');
+
+    // ok, validation error, or failure
+    //header('Status: '.200);
+    header("Status: 200");
+    // return the encoded json
+*/
+
+
+
+
+/*
+header('Access-Control-Allow-Origin: *');
+header('Content-type:application/json;charset=utf-8');
+http_response_code(200);
+    // set the header to make sure cache is forced
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+    header('Content-type:application/json;charset=utf-8');
+header('Status: 200');
+//print_r($_GET);
+$data = array(
+    //'status' => 200,    
+    'hub.challenge' => $_GET['hub_challenge'],
+    //'hub.challenge' => '15f7d1a91c1f40f8a748fd134752feb3',
+);
+
+echo json_encode( $data );
+
+// echo wp_json_encode( $data );
+
+exit();
+*/
+
     }
 
+/*
     public function validate() {
         $message = array(
             'action' => 'error',
@@ -64,5 +132,6 @@ echo "validate webhooks<br>";
 
         return $message;
     }
+*/
 
 }
