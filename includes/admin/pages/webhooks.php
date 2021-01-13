51428c8930c471f84deafe821937a73f950308ca
Erik <erikdmitchell@gmail.com>
2021-01-13 10:31:44 -0500

    removed old text

--- includes/admin/pages/webhooks.php
+++ includes/admin/pages/webhooks.php
@@ -4,27 +4,6 @@
     <h3>Webhooks</h3>
 
 
-Next, you need to create a Webhook Subscription by doing a POST request to the Strava Sync webhook controller (http://website.com/strava-sync/webhook/sync) with a Bearer Token and client_id, client_secret, verify_token, callback_url parameters (The callback_url should be the same as the POST request URL)
-<br>
-check var<br>
-send post<br>
-<p>
-    if get<br>
-    return json<br>
-    else <br>
-    
-    fire webhook
-</p>
-
-<code>
-/*
-function slwp_setup_webhooks() {
-    slwp()->webhooks->create_subscription();
-}
-
-slwp_setup_webhooks();
-*/
-</code>
 
 <p><a href="http://stapi.test/slwp/stravaWebhooks/" target="_blank">Run</a></p>
 
